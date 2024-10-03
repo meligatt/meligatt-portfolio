63741b4058cf116703dedc0c974d1e7a0796beb6
melissa gattoni <meligatt@gmail.com>
2024-10-03 15:38:59 +1000

    replace njk templates with liquid

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,10 +2,16 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig){
     eleventyConfig.addPlugin(eleventyNavigationPlugin);
+    eleventyConfig.setLiquidOptions({
+        jsTruthy: true,
+        strictFilters: false,
+        strictVariables: false,
+    });
     return {
         dir: {
 			input: "src",
-            output: "_site"
+            output: "_site",
+            data: "../_data", 
 		},
     }
 }
